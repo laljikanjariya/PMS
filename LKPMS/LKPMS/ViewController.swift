86b6576900a0ac911147aaf9b59499d2c6cbb01d
Lalji Kanjariya <lalji.kanjariya@siyainfo.com>
2020-05-07 12:06:30 +0530

    LKPMS start

---
+++ LKPMS/LKPMS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LKPMS
+//
+//  Created by Lalji on 07/05/20.
+//  Copyright © 2020 Lalji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
